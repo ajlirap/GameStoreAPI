02910e6840fb1cfaa8bbfa273710a2aca7ec0768
Arturo J Lira Perez <ajlirap@gmail.com>
2024-09-22 15:43:58 -0700

    Refactor logging configuration in Program.cs

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -10,11 +10,6 @@ builder.Services.AddAuthentication().AddJwtBearer();
 
 builder.Services.AddGameStoreAuthorization();
 
-builder.Logging.AddJsonConsole(options => 
-{
-    options.JsonWriterOptions = new() { Indented = true };
-});
-
 var app = builder.Build();
 
 await app.Services.InitializeDbAsync();
